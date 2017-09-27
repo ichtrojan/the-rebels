102955a115b0fad749f9b4d05d5a5e77856e3f6d
Ekpoto Liberty <ekpoto.liberty@gmail.com>
2017-09-27 05:17:13 -0700

    Database configuration setting done

--- __config/database.php
+++ __config/database.php
@@ -4,13 +4,30 @@
  */
 class DBconnect {
 
-	protected $host;
-	protected $user;
-	protected $pass;
-	protected $database;
+	private $host = "localhost";
+	private $user = "root";
+	private $pass = "";
+	private $dbname = "test_db";
+
+	private $dbh;
+	private $error;
 
 	public function __construct() {
-
+		// Set DSN
+		$dsn = 'mysql:host=' . $this->host . ';dbname=' . $this->dbname;
+		// Set options
+		$options = array(
+			PDO::ATTR_PERSISTENT => true,
+			PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+		);
+		// Create a new PDO instanace
+		try {
+			$this->dbh = new PDO($dsn, $this->user, $this->pass, $options);
+		}
+		// Catch any errors
+		 catch (PDOException $e) {
+			$this->error = $e->getMessage();
+		}
 	}
 }
 ?>
