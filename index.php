8d8819fb6f871045c9296531ecfaa7a9845ae977
Ekpoto Liberty <ekpoto.liberty@gmail.com>
2017-09-26 20:02:54 -0700

    loading up

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Third party upload packages..</title>
+	<link rel="stylesheet" type="text/css" href="public/css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="public/fonts/css/font-awesome.css">
+	<script type="text/javascript" src="public/js/jquery.js"></script>
+	<script type="text/javascript" src="public/js/bootstrap.js"></script>
+</head>
+<body>
+	<nav class="navbar navbar-inverse">
+		<div class="container">
+	      <div class="navbar-header">
+	        <a class="navbar-brand" href="#"> <span><img src="img/logo.png" alt=""></span>PaysFund</a>
+	        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false"
+	          aria-controls="navbar">
+	            <span class="sr-only">Toggle navigation</span>
+	            <span class="icon-bar"></span>
+	            <span class="icon-bar"></span>
+	            <span class="icon-bar"></span>
+	          </button>
+	      </div>
+	      <ul class="nav navbar-nav">
+	        <li><a href="/"><i class="fa fa-home"></i> Page 1</a></li>
+	        <li><a href="/"><i class="fa fa-home"></i> page 2</a></li>
+	        <li><a href="/"><i class="fa fa-home"></i> page 3</a></li>
+	        <li><a href="/"><i class="fa fa-home"></i> page 4</a></li>
+	      </ul>
+		</div>
+	</nav>
+
+	<div class="footer">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-6">
+					<span class="small">&copy; 2017 by The Rebels</span>
+				</div>
+			</div>
+		</div>
+	</div>
+
+</body>
+</html>
