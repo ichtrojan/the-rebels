c211cfecc436a3a8a9e153ff5ecfef5f572bf40c
Ekpoto Liberty <ekpoto.liberty@gmail.com>
2017-10-02 11:14:16 +0100

    database refactor

--- __config/processor.php
+++ __config/processor.php
@@ -1,25 +1,32 @@
 <?php
-// inlcude the database class:
-require ("database.php");
-
 /**
 * Upload Packages
 */
 class UploadPackages extends Database
 {
+	protected $host = "localhost";
+	protected $user = "root";
+	protected $pass = "";
+	protected $db = "test_db";
+
 	protected $plug;
 
 	public function __construct() {
-		// call the database construct and plug the connection 
-		parent::__construct();
-		$this->plug = Database::plug();
+		// database connections
+		$this->plug = mysqli_connect($this->host, $this->user, $this->pass, $this->db);
 
+		if(mysqli_connect_error()){
+			return "Fail to connect to databse ";
+		}
 	}
 
 	public function save($name, $desc, $type, $price, $location, $duration, $contact, $images){
 		// tags and dates
 		$tags = "";
-		$date = time();		
+		$date = time();
+
+		// convert to strings
+		$images = implode(',', $images);
 
 		// insert data into the packages table
 		$query = " INSERT INTO packages(NULL, $name, $desc, $type, $price, $location, $duration, $contact, $images, $tags, $date) ";
